50d1b76b8c3e2069b8797aedda15c1cdd1d4aa3c
Superan25 <mastersuperan1881@gmail.com>
2016-06-24 06:31:12 +0000

    Add files via upload

---
+++ test2.java
@@ -0,0 +1,24 @@
+class test2
+{
+	int age;
+	
+	void check_eligibility(int age)
+	{
+		
+		this.age = age;
+		if (this.age >= 18)
+		{
+			System.out.println("The person is eligible to vote ");
+		}
+		
+		else
+		{
+			System.out.println("The person is not eligible to vote ");
+		}
+	}
+	public static void main(String a[])
+	{
+		test2 T = new test2();
+		T.check_eligibility(18); 
+	}
+}
