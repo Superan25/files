029c807a5673317d1c02f91e3cdb460fcfc8a927
Superan25 <mastersuperan1881@gmail.com>
2016-06-27 09:40:57 +0000

    Add files via upload

---
+++ variableDec.java
@@ -0,0 +1,13 @@
+class variableDec
+{
+	int a, b = 4, c;
+	variableDec()
+	{
+	System.out.println("The values of a, b, c are  " + a + "  " + b + "  " + c + "  " + "respectively");	
+	}
+	public static void main(String args[])
+	{
+		variableDec V = new variableDec();	
+	}
+	
+}
