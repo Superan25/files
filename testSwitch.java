ad5bfb95fff88813e82fee33ec0161b8d32e821d
Superan25 <mastersuperan1881@gmail.com>
2016-06-28 10:15:20 +0000

    Add files via upload

---
+++ testSwitch.java
@@ -0,0 +1,23 @@
+class testSwitch
+{
+	public static void main(String a[])
+	{
+		String month = "February";
+		
+		switch(month)
+		{
+			case "January": System.out.println("The number of days in January is 31"); break;
+			case "February": System.out.println("The number of days in February is 28"); break;
+			case "March": System.out.println("The number of days in March is 31"); break;
+			case "April": System.out.println("The number of days in April is 30"); break;
+			case "May": System.out.println("The number of days in May is 31"); break;
+			case "June": System.out.println("The number of days in June is 30"); break;
+			case "July": System.out.println("The number of days in July is 31"); break;
+			case "August": System.out.println("The number of days in August is 31"); break;
+			case "September": System.out.println("The number of days in September is 30"); break;
+			case "October": System.out.println("The number of days in October is 31"); break;
+			case "November": System.out.println("The number of days in November is 30"); break;
+			case "December": System.out.println("The number of days in December is 31"); break;
+		}
+	}
+}
